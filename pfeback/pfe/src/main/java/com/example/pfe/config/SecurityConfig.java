2e393c4f4c38d3fc7f4bace0d5ebc514df3929b0
mohamed islem landolsi <Mohamedislem.landolsi@steps.tn>
2025-06-23 12:08:53 +0100

    back end

---
+++ pfeback/pfe/src/main/java/com/example/pfe/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.pfe.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
